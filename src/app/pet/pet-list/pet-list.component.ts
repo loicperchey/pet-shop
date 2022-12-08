f42bd8096ddcbcb87a4ff04eb17e13eb5210d76d
maximenorval <maximenorval@gmail.com>
2022-12-08 11:22:38 +0100

    5

--- src/app/pet/pet-list/pet-list.component.ts
+++ src/app/pet/pet-list/pet-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { IPet } from '../model/pet';
 
 @Component({
   selector: 'app-pet-list',
   templateUrl: './pet-list.component.html',
-  styleUrls: ['./pet-list.component.scss']
+  styleUrls: ['./pet-list.component.scss'],
 })
 export class PetListComponent implements OnInit {
+  @Input() petList: IPet[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.petList);
   }
-
 }
--- src/app/pet/pet.component.ts
+++ src/app/pet/pet.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IPet, Species } from './model/pet';
 
 @Component({
   selector: 'app-pet',
   templateUrl: './pet.component.html',
   styleUrls: ['./pet.component.scss'],
 })
-export class PetComponent {}
+export class PetComponent implements OnInit {
+  pets: IPet[] = [];
+
+  ngOnInit(): void {
+    this.createPets();
+  }
+
+  private createPets(): void {
+    const names: string[] = ['milou', 'garfield', 'nemo', 'bugs bunny'];
+    const species: Species[] = ['chien', 'chat', 'poisson', 'lapin'];
+    const prices: number[] = [500, 400, 10, 50];
+
+    for (let i = 0; i < 4; i++) {
+      const pet: IPet = {
+        id: i + 1,
+        name: names[i],
+        species: species[i],
+        price: prices[i],
+        isAvailable: i % 2 === 0,
+      };
+
+      this.pets.push(pet);
+    }
+  }
+}
