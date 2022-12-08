d9536d5aae6fca65599b3714c5e799bacc655061
maximenorval <maximenorval@gmail.com>
2022-12-08 09:39:23 +0100

    3

---
+++ src/app/pet/pet-list/pet-list-item/pet-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-list-item',
+  templateUrl: './pet-list-item.component.html',
+  styleUrls: ['./pet-list-item.component.scss']
+})
+export class PetListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
