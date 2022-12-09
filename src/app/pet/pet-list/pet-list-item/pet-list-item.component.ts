23c5d652300b906504b9e2ae20c715a22d98be13
maximenorval <maximenorval@gmail.com>
2022-12-09 13:46:08 +0100

    13

--- src/app/pet/pet-list/pet-list-item/pet-list-item.component.ts
+++ src/app/pet/pet-list/pet-list-item/pet-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { IPet } from '../../model/pet';
+import { PetService } from '../../pet.service';
 
 @Component({
   selector: 'app-pet-list-item',
@@ -10,5 +11,12 @@ import { IPet } from '../../model/pet';
 export class PetListItemComponent {
   @Input() pet!: IPet;
 
-  constructor() {}
+  get selectedPetId(): number {
+    if (!this.petService.selectedPet) {
+      return 0;
+    }
+    return this.petService.selectedPet.id;
+  }
+
+  constructor(private petService: PetService) {}
 }
